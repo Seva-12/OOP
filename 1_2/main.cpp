8f723909c44c9e654930e3d0914ba04df2897ecf
Seva-12 <sevawot3@yandex.ru>
2026-03-11 18:10:06 +0300

    финальный вариант

--- 1_2/main.cpp
+++ 1_2/main.cpp
@@ -1,55 +1,67 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <Windows.h>
+
+std::string BinaryNumber(int EnterNumber)
+    {
+        std::string Binary;
+        int Bufer;
+        int Temp = EnterNumber;
+        for (int i = 0; i < 8; i++) 
+        {
+            Bufer = temp % 2;
+            if (Bufer > 0) 
+            {
+            Binary = Binary + "1";
+            }
+            else
+            {
+                Binary = Binary + "0";
+            }
+            Temp = Temp / 2;
+        }
+        return Binary;
+    }
+
+    int FinalNumber(const std::string& Binary)
+    {
+        int Binary = 0;
+        int Degree = 7;
+
+        for (char c : Binary)
+        {
+            if (c == '1')
+            {
+                Binary = Binary + pow(2, Degree);
+                Degree = Degree - 1;
+            }
+            else
+            {
+                Degree = Degree - 1;
+            }
+        }
+        return Binary;
+    };
 
 int main()
 {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
 	int EnterNumber;
+    std::cout << "Введите число (0-255): ";
 	std::cin >> EnterNumber;
     
     if (EnterNumber >= 0 && EnterNumber <= 256)
     {
-        std::cout << EnterNumber + "\n";
+        std::string Binary = BinaryNumber(EnterNumber);
+    
+        int Decimal = FinalNumber(Binary);
+        std::cout << Decimal;
     } 
     else 
     {
-        std::cout << "ERROR \n";
+        std::cout << "ERROR: число должно быть в диапозоне от 0 до 256!!! \n";
     }
-	
-    std::string BinaryNumber;
-    int bufer;
-    int temp = EnterNumber;
-    for (int i = 0; i < 8; i++) 
-    {
-        bufer = temp % 2;
-        if (bufer > 0) 
-        {
-			BinaryNumber = BinaryNumber + "1";
-        }
-        else
-        {
-            BinaryNumber = BinaryNumber + "0";
-        }
-        temp = temp / 2;
-    }
-    std::cout << BinaryNumber + "\n";
-
-    std::string str;
-    int degree = 7;
-    int FinalNumber = 0;
-
-    for (char c : BinaryNumber)
-    {
-        if (c == '1')
-        {
-            FinalNumber = FinalNumber + pow(2, degree);
-            degree = degree - 1;
-        }
-        else
-        {
-            degree = degree - 1;
-        }
-    }
-    std::cout << FinalNumber;
 
 }
